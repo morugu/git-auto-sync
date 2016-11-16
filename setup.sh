6014f00d6488d488530f52acd298c4f3946a520c
shoya <12c1055@gmail.com>
2016-11-16 21:09:18 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+n/sh
+
+if [ "$1" = "" ]
+then
+    echo "Usage: sh $0 < Repository path like a /var/www >"
+    exit
+fi
+
+DIR_PATH=$1;
+COMMAND="git --git-dir=${DIR_PATH}/.git --work-tree=${DIR_PATH} pull";
+echo $COMMAND;
